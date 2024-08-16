1e295180ddebeba6dd222013096134c74b2f038c
mhe <markus.herhoffer@exentra.de>
2024-08-16 22:07:54 +0200

    ✨ (#10): Add validation REST endpoint for Vereine

---
+++ backend/de.op-paf.vereinsfin.vereine/src/test/java/de/oppaf/vereinsfin/vereine/rest/VereinResourceTestsIT.java
@@ -0,0 +1,7 @@
+package de.oppaf.vereinsfin.vereine.rest;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class VereinResourceTestsIT extends VereinResourceTests {
+}
