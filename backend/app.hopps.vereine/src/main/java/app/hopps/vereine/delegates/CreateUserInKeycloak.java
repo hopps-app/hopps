ccabbad16d2ad9433d8e5d87e3330fe4983d08ad
Emilia Jaser <vapor.schitcrafter@gmail.com>
2024-08-21 19:34:43 +0200

    ✨ Create new user in keycloak

--- backend/app.hopps.vereine/src/main/java/app/hopps/vereine/delegates/CreateUserInKeycloak.java
+++ backend/app.hopps.vereine/src/main/java/app/hopps/vereine/delegates/CreateUserInKeycloak.java
@@ -2,11 +2,80 @@ package app.hopps.vereine.delegates;
 
 import app.hopps.vereine.jpa.Mitglied;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.resource.RealmResource;
+import org.keycloak.admin.client.resource.UsersResource;
+import org.keycloak.representations.idm.RoleRepresentation;
+import org.keycloak.representations.idm.UserRepresentation;
+
+import java.util.List;
 
 @ApplicationScoped
 public class CreateUserInKeycloak {
 
+    Keycloak keycloak;
+    RealmResource realmResource;
+    UsersResource usersResource;
+    RoleRepresentation ownerRole;
+
+    public CreateUserInKeycloak(
+            Keycloak keycloak,
+            @ConfigProperty(name = "app.hopps.vereine.auth.realm-name") String realmName,
+            @ConfigProperty(name = "app.hopps.vereine.auth.default-role") String ownerRoleName
+    ) {
+        this.keycloak = keycloak;
+        this.realmResource = keycloak.realm(realmName);
+        this.usersResource = realmResource.users();
+        this.ownerRole = createOwnerRole(realmResource, ownerRoleName);
+    }
+
+    /**
+     * If necessary, creates the owner role, otherwise just returns it.
+     */
+    private static RoleRepresentation createOwnerRole(RealmResource realmResource, String ownerRoleName) {
+        RoleRepresentation ownerRole;
+        try {
+            ownerRole = realmResource.roles().get(ownerRoleName).toRepresentation();
+        } catch (Exception e) {
+            ownerRole = new RoleRepresentation();
+            ownerRole.setName(ownerRoleName);
+            try {
+                realmResource.roles().create(ownerRole);
+            } catch (Exception ignored) { }
+
+            ownerRole = realmResource.roles().get(ownerRoleName).toRepresentation();
+        }
+        return ownerRole;
+    }
+
     public void createUserInKeycloak(Mitglied user) {
-        // FIXME: Implement me
+        UserRepresentation userRepresentation = new UserRepresentation();
+
+        userRepresentation.setEnabled(true);
+        userRepresentation.setFirstName(user.getFirstName());
+        userRepresentation.setLastName(user.getLastName());
+        userRepresentation.setEmail(user.getEmail());
+        userRepresentation.setUsername(user.getEmail());
+
+        Response response = usersResource.create(userRepresentation);
+
+        if (response.getStatusInfo().getFamily() != Response.Status.Family.SUCCESSFUL) {
+            String body = response.readEntity(String.class);
+            throw new WebApplicationException("Could not create user, body: " + body, response);
+        }
+
+        response.close();
+
+        // Assign user to the owner role
+        UserRepresentation createdUser = usersResource.search(userRepresentation.getUsername())
+                .getFirst();
+
+        usersResource.get(createdUser.getId())
+                .roles()
+                .realmLevel()
+                .add(List.of(ownerRole));
     }
 }
