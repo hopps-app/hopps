5f64d4b1f4a132f0a0772c34001b88567d868e28
mhe <markus.herhoffer@exentra.de>
2024-08-21 13:49:52 +0200

    🚧 (#10): Add stub for Keycloak delegate

---
+++ backend/app.hopps.vereine/src/main/java/app/hopps/vereine/delegates/CreateUserInKeycloak.java
@@ -0,0 +1,12 @@
+package app.hopps.vereine.delegates;
+
+import app.hopps.vereine.jpa.Mitglied;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class CreateUserInKeycloak {
+
+    public void createUserInKeycloak(Mitglied user) {
+        // FIXME: Implement me
+    }
+}
