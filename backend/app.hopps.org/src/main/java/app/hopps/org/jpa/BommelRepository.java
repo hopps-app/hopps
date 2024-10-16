5c3b23a21fd09aff4d7a19df2c55659a2848d007
Emilia Jaser <vapor.schitcrafter@gmail.com>
2024-10-16 17:54:21 +0200

    ✅ (bommel-api): Create tests for simple bommel operations

--- backend/app.hopps.org/src/main/java/app/hopps/org/jpa/BommelRepository.java
+++ backend/app.hopps.org/src/main/java/app/hopps/org/jpa/BommelRepository.java
@@ -2,18 +2,19 @@ package app.hopps.org.jpa;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
+import java.util.Set;
 
 @ApplicationScoped
 public class BommelRepository implements PanacheRepository<Bommel> {
 
-    private static Logger LOGGER = LoggerFactory.getLogger(BommelRepository.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(BommelRepository.class);
 
     /**
      * Fetches the lineage of this bommel,
@@ -38,15 +39,55 @@ public class BommelRepository implements PanacheRepository<Bommel> {
         return parents;
     }
 
+    public Bommel getRoot() {
+        return find("where parent is null").firstResult();
+    }
+
+    @Transactional
+    public void createRoot(Bommel root) {
+        if (root.getParent() != null) {
+            throw new IllegalStateException("Root bommel cannot have a parent");
+        }
+
+        long rootNodeCount = count("where parent is null");
+        if (rootNodeCount != 0) {
+            throw new IllegalStateException("Expected 0 root nodes, found " + rootNodeCount);
+        }
+
+        persist(root);
+    }
+
     /**
-     * Inserts this bommel, linking it to the stored parent (if available).
+     * Inserts this bommel, linking it to the stored parent.
+     * This cannot create a root node, use {@link BommelRepository#createRoot} for that.
      */
-    public void insertBommel(Bommel child) {
+    @Transactional
+    public void insertBommel(Bommel child) throws IllegalStateException, IllegalArgumentException {
+        if (child.getParent() == null) {
+            throw new IllegalArgumentException("Bommel must have a parent");
+        }
+
         if (!child.isPersistent()) {
             child.persist();
         }
 
-        ensureTreeConsistency();
+        ensureNoCycleFromBommel(child);
+    }
+
+    @Transactional
+    public void deleteBommel(Bommel bommel, boolean recursive) {
+        if (!recursive && !bommel.getChildren().isEmpty()) {
+            throw new IllegalArgumentException("Bommel has children, specify recursive=true to delete it and its children");
+        }
+
+        delete(bommel);
+    }
+
+    @Transactional
+    public void moveBommel(Bommel bommel, Bommel destination) {
+        persist(bommel);
+        bommel.setParent(destination);
+        ensureNoCycleFromBommel(bommel);
     }
 
     /**
@@ -55,12 +96,31 @@ public class BommelRepository implements PanacheRepository<Bommel> {
      * have separated and no cycles exist.
      * This is expensive, especially with a large number of bommels.
      */
-    public void ensureTreeConsistency() throws IllegalStateException {
-        var edgesCount = count("where parent != null");
+    @Transactional
+    public void ensureConsistency() throws IllegalStateException {
+        var edgesCount = count("where parent is not null");
         var nodesCount = count();
         if (nodesCount != (edgesCount + 1)) {
             throw new IllegalStateException("Operation introduced loop/created a separate tree");
         }
     }
 
+    /**
+     * Ensures that, starting from child, there is no
+     * cycle in the graph.
+     */
+    private void ensureNoCycleFromBommel(Bommel child) throws IllegalStateException {
+        Set<Long> parentIdSet = new HashSet<>();
+
+        Bommel current = child;
+        while (current != null) {
+            if (parentIdSet.contains(current.id)) {
+                throw new IllegalStateException("Found cycle containing id " + current.id + " starting with " + child.id);
+            }
+
+            parentIdSet.add(current.id);
+
+            current = current.getParent();
+        }
+    }
 }
