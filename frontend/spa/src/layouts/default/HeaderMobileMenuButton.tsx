ec39f0547c947119bbeeae021fa081fc6c4843dc
Kostya Zamurnyak <koss.zam@gmail.com>
2024-10-21 11:51:24 +0300

    :sparkles: vitest with 2 examples

--- frontend/spa/src/layouts/default/HeaderMobileMenuButton.tsx
+++ frontend/spa/src/layouts/default/HeaderMobileMenuButton.tsx
@@ -25,9 +25,9 @@ function HeaderMobileMenuButton() {
                     viewBox="0 0 24 24"
                     fill="none"
                     stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                 >
                     <path d="M18 6 6 18" />
                     <path d="m6 6 12 12" />
---
+++ frontend/spa/src/setupTests.ts
@@ -0,0 +1,18 @@
+import * as matchers from '@testing-library/jest-dom/matchers';
+import { expect, vi } from 'vitest';
+
+expect.extend(matchers);
+
+Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: (query: string) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: vi.fn(), // deprecated
+        removeListener: vi.fn(), // deprecated
+        addEventListener: vi.fn(),
+        removeEventListener: vi.fn(),
+        dispatchEvent: vi.fn(),
+    }),
+});
