ea534cb3985b183f96cc1afa14702a06b4b73b3c
Emilia Jaser <vapor.schitcrafter@gmail.com>
2024-10-22 15:49:40 +0200

    ✨ (bommel-api): Working bommel REST API, to be tested

--- backend/app.hopps.org/src/main/java/app/hopps/org/rest/BommelResource.java
+++ backend/app.hopps.org/src/main/java/app/hopps/org/rest/BommelResource.java
@@ -2,14 +2,20 @@ package app.hopps.org.rest;
 
 import app.hopps.org.jpa.Bommel;
 import app.hopps.org.jpa.BommelRepository;
+import app.hopps.org.jpa.TreeSearchBommel;
 import io.quarkiverse.openfga.client.AuthorizationModelClient;
 import io.quarkiverse.openfga.client.model.TupleKey;
+import io.quarkus.runtime.configuration.ConfigUtils;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.SecurityContext;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Path("/bommel")
 public class BommelResource {
@@ -18,19 +24,83 @@ public class BommelResource {
     BommelRepository bommelRepo;
 
     @Inject
+    SecurityContext securityContext;
+
+    @Inject
     AuthorizationModelClient authModelClient;
+
+    // Auth is only disabled when in dev mode and auth has been disabled through the config property (see below)
+    boolean authEnabled;
+
+    public BommelResource(
+            @ConfigProperty(name = "quarkus.security.auth.enabled-in-dev-mode", defaultValue = "true")
+            boolean devModeAuthEnabled
+    ) {
+        this.authEnabled = devModeAuthEnabled || !ConfigUtils.isProfileActive("dev");
+    }
+
+    @GET
+    @Path("/{id}/children")
+    public Set<Bommel> getBommelChildren(@PathParam("id") long id) {
+        checkUserHasPermission(id, "read");
+
+        Bommel base = bommelRepo.findById(id);
+
+        if (base == null) {
+            throw new WebApplicationException("Invalid id, no such bommel", Response.Status.BAD_REQUEST);
+        }
+
+        return base.getChildren();
+    }
+
+    @GET
+    @Path("/{id}/children/recursive")
+    public List<TreeSearchBommel> getBommelChildrenRecursive(@PathParam("id") long id) {
+        checkUserHasPermission(id, "read");
+        Bommel base = bommelRepo.findById(id);
+
+        if (base == null) {
+            throw new WebApplicationException("Invalid id, no such bommel", Response.Status.BAD_REQUEST);
+        }
+
+        return bommelRepo.getChildrenRecursive(base);
+    }
 
     @GET
     @Path("/{id}")
     public Optional<Bommel> getBommel(@PathParam("id") long id) {
-        authModelClient.check(TupleKey.of("bommel:" + id, "bommel:read", "user:TODO"));
+        checkUserHasPermission(id, "read");
+
         return bommelRepo.findByIdOptional(id);
     }
 
+    @GET
+    @Path("/root")
+    public Optional<Bommel> getRootBommel() {
+        Bommel rootBommel = bommelRepo.getRoot();
+
+        if (rootBommel == null) {
+            return Optional.empty();
+        }
+
+        checkUserHasPermission(rootBommel.id, "read");
+
+        return Optional.of(rootBommel);
+    }
+
     @POST
-    @Path("/create-root")
+    @Path("/root")
     @Transactional
     public Bommel createRoot(Bommel root) {
+        var principal = securityContext.getUserPrincipal();
+
+        if (principal == null && this.authEnabled) {
+            throw new WebApplicationException("User is not logged in", Response.Status.INTERNAL_SERVER_ERROR);
+        }
+
+        // TODO: We're not checking OpenFGA here - not sure what to check for, since there's no
+        //  bommel yet in the tree
+
         return bommelRepo.createRoot(root);
     }
 
@@ -45,6 +115,8 @@ public class BommelResource {
             );
         }
 
+        checkUserHasPermission(bommel.getParent().id, "write");
+
         return bommelRepo.insertBommel(bommel);
     }
 
@@ -52,6 +124,8 @@ public class BommelResource {
     @Path("/{id}")
     @Transactional
     public Bommel updateBommel(Bommel bommel, @PathParam("id") long id) {
+        checkUserHasPermission(id, "write");
+
         var existingBommel = bommelRepo.findById(id);
 
         if (existingBommel == null) {
@@ -74,6 +148,9 @@ public class BommelResource {
     @Path("/move/{id}/to/{newParentId}")
     @Transactional
     public Bommel moveBommel(@PathParam("id") long id, @PathParam("newParentId") long newParentId) {
+        checkUserHasPermission(id, "write");
+        checkUserHasPermission(newParentId, "write");
+
         Bommel base = bommelRepo.findById(id);
         Bommel parent = bommelRepo.findById(newParentId);
 
@@ -97,7 +174,9 @@ public class BommelResource {
     @DELETE
     @Path("/{id}")
     @Transactional
-    public Bommel deleteBommel(@PathParam("id") long id, @QueryParam("recursive") @DefaultValue("false") boolean recursive) {
+    public void deleteBommel(@PathParam("id") long id, @QueryParam("recursive") @DefaultValue("false") boolean recursive) {
+        checkUserHasPermission(id, "write");
+
         Bommel base = bommelRepo.findById(id);
 
         if (base == null) {
@@ -108,8 +187,24 @@ public class BommelResource {
         }
 
         bommelRepo.deleteBommel(base, recursive);
-
-        return base;
     }
 
+    /**
+     * Checks that the currently signed in user can access this bommel with this relation.
+     * Throws a WebApplication exception if anything goes wrong.
+     */
+    private void checkUserHasPermission(long bommelId, String relation) throws WebApplicationException {
+        var principal = securityContext.getUserPrincipal();
+
+        if (principal == null && this.authEnabled) {
+            throw new WebApplicationException("User is not logged in", Response.Status.INTERNAL_SERVER_ERROR);
+        }
+
+        String username = principal == null ? "anonymous" : principal.getName();
+
+        var accessTuple = TupleKey.of("bommel:" + bommelId, relation, "user:" + username);
+        if (this.authEnabled && !authModelClient.check(accessTuple).await().indefinitely()) {
+            throw new WebApplicationException(Response.Status.UNAUTHORIZED);
+        }
+    }
 }
