96bff9c6a5a9db95a1117eebd7244b9aa3f28cb3
Johsiko <joshi.ko@t-online.de>
2024-10-28 20:47:45 +0100

    Preparation of Dataclasses

--- backend/app.hopps.zugferd-document-service/src/main/java/app/hopps/ZugFerdService.java
+++ backend/app.hopps.zugferd-document-service/src/main/java/app/hopps/ZugFerdService.java
@@ -1,4 +1,15 @@
 package app.hopps;
+import jakarta.enterprise.context.ApplicationScoped;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+@ApplicationScoped
 public class ZugFerdService {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(ZugFerdService.class);
+
+    @ConfigProperty(name = "app.hopps.zugferd-service.eInvoiceId")
+    String eInvoiceId;
+
 }
---
+++ backend/app.hopps.zugferd-document-service/src/main/java/app/hopps/model/InvoiceData.java
@@ -0,0 +1,35 @@
+package app.hopps.model;
+import org.mustangproject.Invoice;
+import org.mustangproject.ZUGFeRD.TransactionCalculator;
+import java.util.Date;
+import java.util.Optional;
+
+public record InvoiceData(
+        Optional<String> customerName,
+        Optional<String> billingAddress,
+        Optional<String> purchaseOrderNumber,
+        Optional<String> invoiceId,
+        Date invoiceDate,
+        Optional<Date> dueDate,
+        double total,
+        Optional<Double> amountDue,
+        String currencyCode) {
+
+
+    public static InvoiceData fromZugferd(Invoice invoice) {
+
+        TransactionCalculator tc = new TransactionCalculator(invoice);
+        return new InvoiceData(
+                Optional.ofNullable(invoice.getRecipient().getName()),
+                Optional.ofNullable(invoice.getRecipient().getAdditionalAddress()),
+                Optional.ofNullable(invoice.getReferenceNumber()),
+                Optional.ofNullable(invoice.getNumber()),
+                invoice.getIssueDate(),
+                Optional.ofNullable(invoice.getDueDate()),
+                tc.getChargeTotal().doubleValue(),
+                Optional.ofNullable(tc.getChargeTotal().subtract(invoice.getTotalPrepaidAmount()).doubleValue()),
+                invoice.getCurrency()
+
+        );
+    }
+}
