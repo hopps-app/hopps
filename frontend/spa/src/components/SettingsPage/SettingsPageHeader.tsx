0ad60d7edbde3212cef8e9f280915b86a6320ef9
Kostya Zamurnyak <koss.zam@gmail.com>
2024-11-07 19:34:38 +0200

    [126] settings page and organization structure

---
+++ frontend/spa/src/components/SettingsPage/SettingsPageHeader.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import Icon from '@/components/ui/Icon.tsx';
+import { useActiveTab } from '@/context/ActiveTabContext.tsx';
+
+interface SettingsPageHeaderProps {
+    children?: React.ReactNode;
+}
+
+function SettingsPageHeader(props: SettingsPageHeaderProps) {
+    const { activeTab } = useActiveTab();
+
+    return (
+        <>
+            <div className="flex flex-row justify-between">
+                <h1 className="flex flex-row gap-2 items-center h-10">
+                    {activeTab?.icon && <Icon icon={activeTab.icon} size="md" />}
+                    {activeTab?.title}
+                </h1>
+
+                <div>{props.children}</div>
+            </div>
+            <div className="py-4">
+                <hr />
+            </div>
+        </>
+    );
+}
+
+export default SettingsPageHeader;
