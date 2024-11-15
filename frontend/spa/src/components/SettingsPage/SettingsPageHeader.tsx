121a7c2fd17300bafbe13da86fac297e1fddb028
Kostya Zamurnyak <koss.zam@gmail.com>
2024-11-15 13:14:00 +0200

    [126] fix ts issues for tests, add refresing token, add some tests and improvements

--- frontend/spa/src/components/SettingsPage/SettingsPageHeader.tsx
+++ frontend/spa/src/components/SettingsPage/SettingsPageHeader.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Icon from '@/components/ui/Icon.tsx';
-import { useActiveTab } from '@/context/ActiveTabContext.tsx';
+import { useActiveTab } from '@/context/ActiveTabContext.ts';
 
 interface SettingsPageHeaderProps {
     children?: React.ReactNode;
--- frontend/spa/src/hooks/use-toast.ts
+++ frontend/spa/src/hooks/use-toast.ts
@@ -12,13 +12,6 @@ type ToasterToast = ToastProps & {
     action?: ToastActionElement;
 };
 
-const actionTypes = {
-    ADD_TOAST: 'ADD_TOAST',
-    UPDATE_TOAST: 'UPDATE_TOAST',
-    DISMISS_TOAST: 'DISMISS_TOAST',
-    REMOVE_TOAST: 'REMOVE_TOAST',
-} as const;
-
 let count = 0;
 
 function genId() {
@@ -26,23 +19,28 @@ function genId() {
     return count.toString();
 }
 
-type ActionType = typeof actionTypes;
+enum ActionType {
+    ADD_TOAST = 'ADD_TOAST',
+    UPDATE_TOAST = 'UPDATE_TOAST',
+    DISMISS_TOAST = 'DISMISS_TOAST',
+    REMOVE_TOAST = 'REMOVE_TOAST',
+} //'ADD_TOAST' | 'UPDATE_TOAST' | 'DISMISS_TOAST' | 'REMOVE_TOAST';
 
 type Action =
     | {
-          type: ActionType['ADD_TOAST'];
+          type: ActionType.ADD_TOAST;
           toast: ToasterToast;
       }
     | {
-          type: ActionType['UPDATE_TOAST'];
+          type: ActionType.UPDATE_TOAST;
           toast: Partial<ToasterToast>;
       }
     | {
-          type: ActionType['DISMISS_TOAST'];
+          type: ActionType.DISMISS_TOAST;
           toastId?: ToasterToast['id'];
       }
     | {
-          type: ActionType['REMOVE_TOAST'];
+          type: ActionType.REMOVE_TOAST;
           toastId?: ToasterToast['id'];
       };
 
