95dce00ddf7b9699d9419a3f6c2d5b9d55596015
Manuel Klaus <code@codeflowwizardry.de>
2024-11-30 13:29:19 +0100

    ✅ (109): Add pact consumer test

---
+++ backend/app.hopps.fin/src/test/java/app/hopps/fin/pact/PactTestProfile.java
@@ -0,0 +1,15 @@
+package app.hopps.fin.pact;
+
+import io.quarkus.test.junit.QuarkusTestProfile;
+
+import java.util.Map;
+
+public class PactTestProfile implements QuarkusTestProfile {
+    @Override
+    public Map<String, String> getConfigOverrides() {
+
+        return Map.of(
+                "quarkus.rest-client.org-service.uri", "http://localhost:${app.hopps.fin.pact.port}",
+                "app.hopps.fin.pact.port", "57852");
+    }
+}
