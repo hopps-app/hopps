d54b7a43edce87d4bd680de172b07804c0c494c6
Johsiko <joshi.ko@t-online.de>
2024-12-05 06:17:29 +0100

    update to KafkaConnector.java to suit Inputstreams

--- backend/app.hopps.zugferd-document-service/src/main/java/app/hopps/KafkaConnector.java
+++ backend/app.hopps.zugferd-document-service/src/main/java/app/hopps/KafkaConnector.java
@@ -1,17 +1,25 @@
 package app.hopps;
 
-import app.hopps.model.InvoiceDocument;
+import app.hopps.model.InvoiceData;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
+
+import javax.xml.xpath.XPathExpressionException;
+import java.io.InputStream;
+import java.text.ParseException;
 
 @ApplicationScoped
 public class KafkaConnector {
 
-    @Incoming("source")
-    @Outgoing("sink")
-    public String process(InvoiceDocument invoiceDocument) {
+    @Inject
+    ZugFerdService zugFerdService;
+
+    @Incoming("invoice-document-in")
+    @Outgoing("invoice-document-out")
+    public InvoiceData process(InputStream inputStream) throws XPathExpressionException, ParseException {
         // Process the incoming message payload and return an updated payload
-        return invoiceDocument.URL().toUpperCase();
+        return zugFerdService.scanInvoice(inputStream);
     }
 }
