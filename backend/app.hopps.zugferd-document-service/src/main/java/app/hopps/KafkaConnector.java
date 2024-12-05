5aac8015eb1e51ec5466cd307f6c0aee7815cfa2
Johsiko <joshi.ko@t-online.de>
2024-12-05 06:18:02 +0100

    Revert "update to KafkaConnector.java to suit Inputstreams"
    This reverts commit d54b7a43edce87d4bd680de172b07804c0c494c6.

--- backend/app.hopps.zugferd-document-service/src/main/java/app/hopps/KafkaConnector.java
+++ backend/app.hopps.zugferd-document-service/src/main/java/app/hopps/KafkaConnector.java
@@ -1,25 +1,17 @@
 package app.hopps;
 
-import app.hopps.model.InvoiceData;
+import app.hopps.model.InvoiceDocument;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
-
-import javax.xml.xpath.XPathExpressionException;
-import java.io.InputStream;
-import java.text.ParseException;
 
 @ApplicationScoped
 public class KafkaConnector {
 
-    @Inject
-    ZugFerdService zugFerdService;
-
-    @Incoming("invoice-document-in")
-    @Outgoing("invoice-document-out")
-    public InvoiceData process(InputStream inputStream) throws XPathExpressionException, ParseException {
+    @Incoming("source")
+    @Outgoing("sink")
+    public String process(InvoiceDocument invoiceDocument) {
         // Process the incoming message payload and return an updated payload
-        return zugFerdService.scanInvoice(inputStream);
+        return invoiceDocument.URL().toUpperCase();
     }
 }
