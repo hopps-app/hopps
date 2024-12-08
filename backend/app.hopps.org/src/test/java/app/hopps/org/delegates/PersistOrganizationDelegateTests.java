4789183a1fcc63b825332fc21066ce4860da9b20
Markus Herhoffer <markus@herhoffer.net>
2024-12-08 19:17:48 +0100

    ✨ (#177): Add PersistOrganizationDelegate and its tests
    Implemented PersistOrganizationDelegate to handle the persistence of organizations, their owners, and root Bommels. Added unit tests to verify that organizations, members, and root Bommels are correctly persisted, ensuring integrity and expected behavior. Introduced a default emoji for root Bommels to standardize their representation.

---
+++ backend/app.hopps.org/src/test/java/app/hopps/org/delegates/PersistOrganizationDelegateTests.java
@@ -0,0 +1,62 @@
+package app.hopps.org.delegates;
+
+import app.hopps.org.jpa.*;
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.collection.IsIterableWithSize.iterableWithSize;
+
+@QuarkusTest
+public class PersistOrganizationDelegateTests {
+
+    @Inject
+    PersistOrganizationDelegate persistOrganizationDelegate;
+
+    @Inject
+    BommelRepository bommelRepository;
+
+    @Inject
+    OrganizationRepository organizationRepository;
+
+    @Inject
+    MemberRespository memberRepository;
+
+    @BeforeEach
+    @Transactional
+    void cleanupDB()
+    {
+        bommelRepository.deleteAll();
+        organizationRepository.deleteAll();
+        memberRepository.deleteAll();
+    }
+
+    @Test
+    void shouldCreateRootBommel()
+    {
+        // given
+        Organization kegelclub = new Organization();
+        kegelclub.setName("Kegelklub 777");
+        kegelclub.setType(Organization.TYPE.EINGETRAGENER_VEREIN);
+        kegelclub.setSlug("kegelklub-777");
+
+        Member kevin = new Member();
+        kevin.setFirstName("Kevin");
+        kevin.setLastName("Kegelkönig");
+        kevin.setEmail("pinking777@gmail.com");
+
+        // when
+        persistOrganizationDelegate.persistOrg(kegelclub, kevin);
+
+        // then
+        assertThat(organizationRepository.listAll(), iterableWithSize(1));
+        assertThat(bommelRepository.listAll(), iterableWithSize(1));
+        assertThat(bommelRepository.listAll(), hasItem(hasProperty("emoji", equalTo("\uD83C\uDF33"))));
+    }
+}
