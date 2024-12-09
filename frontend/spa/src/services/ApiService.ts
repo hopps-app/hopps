0743da0db872292c682aff0c663e66293565f735
Kostya <koss.zam@gmail.com>
2024-12-09 13:09:06 +0000

    [000] create organization WIP

--- frontend/spa/src/services/ApiService.ts
+++ frontend/spa/src/services/ApiService.ts
@@ -1,17 +1,20 @@
 import { BommelService } from '@/services/api/BommelService.ts';
 import { InvoicesService } from '@/services/api/invoicesService.ts';
+import { OrganizationService } from '@/services/api/OrganizationService.ts';
 
 export class ApiService {
     public baseUrl: string;
 
     public bommel: BommelService;
     public invoices: InvoicesService;
+    public organization: OrganizationService;
 
     constructor() {
         this.baseUrl = import.meta.env.VITE_API_URL || '';
 
         this.bommel = new BommelService(this.baseUrl);
         this.invoices = new InvoicesService(this.baseUrl);
+        this.organization = new OrganizationService(this.baseUrl);
     }
 }
 
