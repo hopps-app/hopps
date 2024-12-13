ce7a03a5701c821baf3c8c97b39887c180e856e1
Kostya Zamurnyak <koss.zam@gmail.com>
2024-12-13 20:39:39 +0200

    [000] add translations and ability to change languages

--- frontend/spa/src/components/views/ProfileSettingsView.tsx
+++ frontend/spa/src/components/views/ProfileSettingsView.tsx
@@ -1,16 +1,26 @@
 import { useCallback, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 
 import themeService, { Themes } from '@/services/ThemeService.ts';
 import Radio from '@/components/ui/Radio.tsx';
 import SettingsPageHeader from '@/components/SettingsPage/SettingsPageHeader.tsx';
+import Select from '@/components/ui/Select.tsx';
+import languageService from '@/services/LanguageService.ts';
 
 function ProfileSettingsView() {
-    const [themes] = useState([
+    const { t } = useTranslation();
+    const themes = [
         { label: 'Light', value: 'light' },
         { label: 'Dark', value: 'dark' },
         { label: 'Auto', value: 'auto' },
-    ]);
+    ];
+    const languages = [
+        { label: 'English', value: 'en' },
+        { label: 'German', value: 'de' },
+        { label: 'Ukrainian', value: 'uk' },
+    ];
     const [theme, setTheme] = useState(themeService.isAutoMode() ? 'auto' : themeService.getTheme());
+    const [currentLanguage, setCurrentLanguage] = useState(languageService.getLanguage());
 
     const onThemeChanged = useCallback((value: string) => {
         setTheme(value);
@@ -22,20 +32,27 @@ function ProfileSettingsView() {
         }
     }, []);
 
+    const onLanguageChanged = useCallback((value: string) => {
+        setCurrentLanguage(value);
+        languageService.setLanguage(value);
+    }, []);
+
     return (
         <>
             <SettingsPageHeader />
             <div>
                 <div className="mt-4">
                     <div className="flex flex-row">
-                        <div className="min-w-[200px]">Theme:</div>
+                        <div className="min-w-[200px]">{t('profile.settings.theme')}:</div>
                         <div>
                             <Radio items={themes} value={theme || 'auto'} layout="horizontal" onValueChange={onThemeChanged} />
                         </div>
                     </div>
-                    <div className="flex flex-row">
-                        <div className="min-w-[200px]">Other settings....</div>
-                        <div>...</div>
+                    <div className="flex flex-row my-2">
+                        <div className="min-w-[200px] leading-10">{t('profile.settings.language')}:</div>
+                        <div>
+                            <Select items={languages} value={currentLanguage} onValueChanged={onLanguageChanged} />
+                        </div>
                     </div>
                 </div>
             </div>
