cbca0c852078b91c4fd4ddcc8d84b130d0bdefd0
Kostya <koss.zam@gmail.com>
2024-12-13 10:11:26 +0000

    [000] create organization create root bommel

--- frontend/spa/src/components/views/OrganizationSettingsView.tsx
+++ frontend/spa/src/components/views/OrganizationSettingsView.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import OrganizationTree from '@/components/OrganizationStructureTree/OrganizationTree.tsx';
@@ -6,14 +6,19 @@ import Button from '@/components/ui/Button.tsx';
 import SettingsPageHeader from '@/components/SettingsPage/SettingsPageHeader.tsx';
 import { OrganizationTreeNodeModel } from '@/components/OrganizationStructureTree/OrganizationTreeNodeModel.ts';
 import { useToast } from '@/hooks/use-toast.ts';
+import apiService from '@/services/ApiService.ts';
+import { useStore } from '@/store/store.ts';
+import { Bommel } from '@/services/api/types/Bommel.ts';
 
 function OrganizationSettingsView() {
     const { toast } = useToast();
     const { t } = useTranslation();
-    const savedTree = localStorage.getItem('organizationTree');
-    const saveTreeNodes = savedTree ? (JSON.parse(savedTree) as OrganizationTreeNodeModel[]) : [];
-
-    const [tree, setTree] = useState<OrganizationTreeNodeModel[]>(saveTreeNodes);
+    const store = useStore();
+    const [isOrganizationError, setIsOrganizationError] = useState(false);
+    // const savedTree = localStorage.getItem('organizationTree');
+    // const saveTreeNodes = savedTree ? (JSON.parse(savedTree) as OrganizationTreeNodeModel[]) : [];
+    const [rootBommel, setRootBommel] = useState<Bommel | null>(null);
+    const [tree, setTree] = useState<OrganizationTreeNodeModel[]>([]);
 
     const onTreeChanged = (newTree: OrganizationTreeNodeModel[]) => {
         setTree(newTree);
@@ -25,6 +30,49 @@ function OrganizationSettingsView() {
         toast({ title: t('organizationSettings.saved'), variant: 'success' });
     };
 
+    useEffect(() => {
+        console.log('rootBommel changed', rootBommel);
+        if (!rootBommel) {
+            setTree([]);
+            return;
+        }
+    }, [rootBommel]);
+
+    useEffect(() => {
+        const organization = store.organization;
+
+        if (!organization?.id) {
+            setIsOrganizationError(true);
+            return;
+        }
+
+        const loadRootBommel = async () => {
+            return await apiService.bommel.getRootBommel(organization.id);
+        };
+        const createRootBommel = async () => {
+            return await apiService.bommel.createRootBommel({
+                organization: { id: organization.id },
+                name: 'root',
+                emoji: '',
+                children: [],
+                parent: undefined,
+            });
+        };
+
+        loadRootBommel().then(async (rootBommel) => {
+            console.log('ROOT', rootBommel);
+
+            if (rootBommel) {
+                setRootBommel(rootBommel);
+                return;
+            }
+
+            await createRootBommel().then((rootBommel) => {
+                setRootBommel(rootBommel);
+            });
+        });
+    }, []);
+
     return (
         <>
             <SettingsPageHeader>
@@ -32,7 +80,7 @@ function OrganizationSettingsView() {
             </SettingsPageHeader>
 
             <h3>Structure:</h3>
-            <OrganizationTree tree={tree} onTreeChanged={onTreeChanged} />
+            {isOrganizationError ? <div>Error</div> : <OrganizationTree tree={tree} onTreeChanged={onTreeChanged} />}
         </>
     );
 }
