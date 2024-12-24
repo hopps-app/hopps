94241b180dd4b0b42fac762feeb2faea0a7f334e
josh1ko <joshi.ko@t-online.de>
2024-12-24 01:09:21 +0100

    Removement of unused imports

--- backend/app.hopps.zugferd-document-service/src/test/java/app.hopps/ZugFerdServiceTest.java
+++ backend/app.hopps.zugferd-document-service/src/test/java/app.hopps/ZugFerdServiceTest.java
@@ -3,7 +3,6 @@ package app.hopps;
 import app.hopps.model.InvoiceData;
 import io.quarkus.test.junit.QuarkusTest;
 import jakarta.inject.Inject;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import java.io.InputStream;
