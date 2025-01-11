2a364a4129cc0490a85cca6739e828670a5b4648
Manuel Klaus <code@codeflowwizardry.de>
2025-01-12 00:19:17 +0100

    ✅ (#236): Add tests

--- backend/app.hopps.mailservice/src/main/java/app/hopps/mailservice/Mail.java
+++ backend/app.hopps.mailservice/src/main/java/app/hopps/mailservice/Mail.java
@@ -1,15 +1,23 @@
 package app.hopps.mailservice;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.util.Arrays;
 import java.util.Map;
 import java.util.Objects;
 
 public record Mail(String[] mailReceivers, MailTemplates templateId, Map<String, String> variables) {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
+        if (o == null || getClass() != o.getClass())
+            return false;
         Mail mail = (Mail) o;
-        return Objects.deepEquals(mailReceivers, mail.mailReceivers) && templateId == mail.templateId && Objects.equals(variables, mail.variables);
+        return Objects.deepEquals(mailReceivers, mail.mailReceivers) && templateId == mail.templateId
+                && Objects.equals(variables, mail.variables);
     }
 
     @Override
@@ -19,10 +27,14 @@ public record Mail(String[] mailReceivers, MailTemplates templateId, Map<String,
 
     @Override
     public String toString() {
-        return "Mail{" +
-                "mailReceivers=" + Arrays.toString(mailReceivers) +
-                ", templateId=" + templateId +
-                ", variables=" + variables +
-                '}';
+        try {
+            return objectMapper.writeValueAsString(this);
+        } catch (JsonProcessingException ignored) {
+            return "{" +
+                    "mailReceivers: [" + String.join(",", mailReceivers) + "]," +
+                    "templateId: " + templateId + ',' +
+                    "variables: " + variables +
+                    '}';
+        }
     }
 }
---
+++ backend/app.hopps.mailservice/src/test/java/app/hopps/mailservice/MailTest.java
@@ -0,0 +1,26 @@
+package app.hopps.mailservice;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MailTest {
+    @Test
+    void testEquals() {
+        EqualsVerifier.forClass(Mail.class).verify();
+    }
+
+    @Test
+    void testToString() {
+        Mail mail = new Mail(new String[] { "info@hopps.de", "support@hopps.de" }, MailTemplates.TEMP, Map.of());
+
+        String expected = "{\"mailReceivers\":[\"info@hopps.de\",\"support@hopps.de\"],\"templateId\":\"TEMP\",\"variables\":{}}";
+
+        String actual = mail.toString();
+
+        assertEquals(expected, actual);
+    }
+}
