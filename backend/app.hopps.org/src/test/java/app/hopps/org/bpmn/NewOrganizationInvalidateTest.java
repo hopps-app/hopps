7a288931fadbf5b21a3dc8b7af9314243c62073d
Emilia Jaser <vapor.schitcrafter@gmail.com>
2025-01-23 10:42:18 +0100

    🎨 (general): Fix SonarQube issues in fin-narrator and org

--- backend/app.hopps.org/src/test/java/app/hopps/org/bpmn/NewOrganizationInvalidateTest.java
+++ backend/app.hopps.org/src/test/java/app/hopps/org/bpmn/NewOrganizationInvalidateTest.java
@@ -20,7 +20,6 @@ import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.ArgumentMatchers.any;
 
 @QuarkusTest
 class NewOrganizationInvalidateTest {
--- backend/app.hopps.org/src/test/java/app/hopps/org/bpmn/NewOrganizationTests.java
+++ backend/app.hopps.org/src/test/java/app/hopps/org/bpmn/NewOrganizationTests.java
@@ -18,8 +18,6 @@ import org.kie.kogito.process.ProcessInstance;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
