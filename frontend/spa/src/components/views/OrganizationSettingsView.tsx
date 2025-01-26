2e9e2e96adce805a7f9c0b5ea2c06cda668a1841
Kostya Zamurnyak <koss.zam@gmail.com>
2025-01-26 20:40:26 +0200

    autosaving bommels

--- frontend/spa/src/components/views/OrganizationSettingsView.tsx
+++ frontend/spa/src/components/views/OrganizationSettingsView.tsx
@@ -2,21 +2,21 @@ import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import OrganizationTree from '@/components/OrganizationStructureTree/OrganizationTree.tsx';
-import Button from '@/components/ui/Button.tsx';
 import SettingsPageHeader from '@/components/SettingsPage/SettingsPageHeader.tsx';
 import { OrganizationTreeNodeModel } from '@/components/OrganizationStructureTree/OrganizationTreeNodeModel.ts';
-import { useToast } from '@/hooks/use-toast.ts';
 import { useStore } from '@/store/store.ts';
 import { Bommel } from '@/services/api/types/Bommel.ts';
 import organizationTreeService from '@/services/OrganizationTreeService.ts';
+import apiService from '@/services/ApiService.ts';
+import { LoadingOverlay } from '@/components/ui/LoadingOverlay.tsx';
+import { useToast } from '@/hooks/use-toast.ts';
 
 function OrganizationSettingsView() {
-    const { showSuccess, showError } = useToast();
     const { t } = useTranslation();
+    const { showError } = useToast();
     const store = useStore();
     const [isOrganizationError, setIsOrganizationError] = useState(false);
     const [isLoading, setIsLoading] = useState(true);
-    const [originalBommels, setOriginalBommels] = useState<Map<number, Bommel>>(new Map());
     const [rootBommel, setRootBommel] = useState<Bommel | null>(null);
     const [tree, setTree] = useState<OrganizationTreeNodeModel[]>([]);
 
@@ -24,27 +24,69 @@ function OrganizationSettingsView() {
         const bommels = await organizationTreeService.getOrganizationBommels(rootBommel!.id!);
         const nodes = organizationTreeService.bommelsToTreeNodes(bommels, rootBommel!.id);
 
-        setOriginalBommels(new Map<number, Bommel>(bommels.map((bommel) => [bommel.id, bommel])));
         setTree(nodes);
     };
 
-    const onTreeChanged = (newTree: OrganizationTreeNodeModel[]) => {
-        setTree(newTree);
-    };
-
-    const onClickSave = async () => {
-        setIsLoading(true);
-        const sortedTree = organizationTreeService.sortTreeByDepth(tree);
+    const createTreeNode = async () => {
         try {
-            await organizationTreeService.saveOrganizationTree(sortedTree, rootBommel!.id!, originalBommels);
-            await loadTree();
-            showSuccess(t('organization.settings.saved'));
+            const node = await apiService.bommel.createBommel({
+                name: 'New item',
+                emoji: 'grey_question',
+                children: [],
+                parent: { id: rootBommel!.id },
+            });
+            return organizationTreeService.bommelsToTreeNodes([node], rootBommel!.id)[0];
         } catch (e) {
             console.error(e);
-            showError(t('organization.settings.saveError'));
-        } finally {
-            setIsLoading(false);
+            showError('Failed to create.');
         }
+    };
+    const updateTreeNode = async (node: OrganizationTreeNodeModel) => {
+        let isSuccess = false;
+
+        try {
+            await apiService.bommel.updateBommel(node.id as number, {
+                name: node.text,
+                emoji: node.data?.emoji,
+                parent: { id: node.parent as number },
+            });
+            isSuccess = true;
+        } catch (e) {
+            console.error(e);
+            showError('Failed to update.');
+        }
+
+        return isSuccess;
+    };
+
+    const moveTreeNode = async (node: OrganizationTreeNodeModel) => {
+        let isSuccess = false;
+        setIsLoading(true);
+        try {
+            const parent = node.parent ? node.parent : rootBommel!.id;
+            await apiService.bommel.moveBommel(node.id as number, parent as number);
+            isSuccess = true;
+        } catch (e) {
+            console.error(e);
+            showError('Failed to move bommel.');
+        }
+        setIsLoading(false);
+
+        return isSuccess;
+    };
+
+    const deleteTreeNode = async (id: string | number) => {
+        setIsLoading(true);
+        try {
+            await apiService.bommel.deleteBommel(id as number);
+            await loadTree();
+            return true;
+        } catch (e) {
+            console.error(e);
+            showError('Failed to delete.');
+        }
+        setIsLoading(false);
+        return false;
     };
 
     useEffect(() => {
@@ -77,20 +119,15 @@ function OrganizationSettingsView() {
 
     return (
         <>
-            <SettingsPageHeader>
-                {!isOrganizationError && (
-                    <Button onClick={onClickSave} disabled={isLoading}>
-                        Save
-                    </Button>
-                )}
-            </SettingsPageHeader>
+            <SettingsPageHeader />
+            {isLoading && <LoadingOverlay />}
 
             {isOrganizationError ? (
                 <div>{t('organization.settings.error')}</div>
             ) : (
                 <>
                     <h3>Structure:</h3>
-                    <OrganizationTree tree={tree} onTreeChanged={onTreeChanged} />
+                    <OrganizationTree tree={tree} createNode={createTreeNode} updateNode={updateTreeNode} deleteNode={deleteTreeNode} moveNode={moveTreeNode} />
                 </>
             )}
         </>
