0fe2660f39f008bf9f0587d913b28340cf533653
Manuel Klaus <code@codeflowwizardry.de>
2025-01-29 15:41:15 +0100

    ✨ (#269): Introducing OpenFGA to FIN-Service

--- backend/app.hopps.fin/src/main/java/app/hopps/fin/endpoint/TransactionRecordResource.java
+++ backend/app.hopps.fin/src/main/java/app/hopps/fin/endpoint/TransactionRecordResource.java
@@ -1,6 +1,8 @@
 package app.hopps.fin.endpoint;
 
+import app.hopps.fin.S3Handler;
 import app.hopps.fin.client.OrgRestClient;
+import app.hopps.fin.fga.FgaProxy;
 import app.hopps.fin.jpa.TransactionRecordRepository;
 import app.hopps.fin.jpa.entities.TransactionRecord;
 import io.quarkus.panache.common.Page;
@@ -15,10 +17,12 @@ import jakarta.ws.rs.NotFoundException;
 import jakarta.ws.rs.PATCH;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.SecurityContext;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
@@ -26,20 +30,34 @@ import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.jboss.resteasy.reactive.ClientWebApplicationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import software.amazon.awssdk.services.s3.model.NoSuchKeyException;
 
+import java.io.InputStream;
 import java.util.List;
 import java.util.Optional;
 
 @Authenticated
 @Path("")
 public class TransactionRecordResource {
+    private static final Logger LOG = LoggerFactory.getLogger(TransactionRecordResource.class);
+
     private final TransactionRecordRepository repository;
     private final OrgRestClient orgRestClient;
+    private final S3Handler s3Handler;
+    private final FgaProxy fgaProxy;
+    private final SecurityContext securityContext;
 
     @Inject
-    public TransactionRecordResource(TransactionRecordRepository repository, @RestClient OrgRestClient orgRestClient) {
+    public TransactionRecordResource(TransactionRecordRepository repository,
+            @RestClient OrgRestClient orgRestClient,
+            S3Handler s3Handler, FgaProxy fgaProxy, SecurityContext securityContext) {
         this.repository = repository;
         this.orgRestClient = orgRestClient;
+        this.s3Handler = s3Handler;
+        this.fgaProxy = fgaProxy;
+        this.securityContext = securityContext;
     }
 
     @GET
@@ -51,14 +69,35 @@ public class TransactionRecordResource {
         Page page = new Page(parameters.getPageIndex(), parameters.getPageSize());
 
         if (parameters.getDetached().isPresent()) {
+            // Records without bommelId have no permissions
             return repository.findWithoutBommel(page);
         }
 
-        Optional<Long> bommelId = parameters.getBommelId();
-        if (bommelId.isPresent()) {
-            return repository.findByBommelId(bommelId.get(), page);
+        Optional<Long> bommelIdOpt = parameters.getBommelId();
+        if (bommelIdOpt.isPresent()) {
+            Long bommelId = bommelIdOpt.get();
+            verifyPermission(bommelId);
+            return repository.findByBommelId(bommelId, page);
         } else {
-            return repository.findAll().page(page).list();
+            List<Long> accessibleBommels = fgaProxy.getAccessibleBommels(securityContext.getUserPrincipal().getName());
+            return repository.findAll(accessibleBommels).page(page).list();
+        }
+    }
+
+    @GET
+    @Path("/{id}/document")
+    @Produces(MediaType.APPLICATION_OCTET_STREAM)
+    public InputStream getDocumentByKey(@PathParam("id") Long transactionId) {
+        TransactionRecord transactionRecord = getTransactionRecordAndVerify(transactionId);
+
+        String documentKey = transactionRecord.getDocumentKey();
+        try {
+            return s3Handler.getFile(documentKey);
+        } catch (NoSuchKeyException ignored) {
+            LOG.info("File with key {} not found", documentKey);
+            throw new NotFoundException(Response.status(Response.Status.NOT_FOUND)
+                    .entity("Document with key " + documentKey + " not found")
+                    .build());
         }
     }
 
@@ -70,15 +109,8 @@ public class TransactionRecordResource {
     @APIResponse(responseCode = "404", description = "Specified transaction record id was not found", content = @Content(mediaType = MediaType.TEXT_PLAIN))
     @APIResponse(responseCode = "400", description = "Bommel was not found", content = @Content(mediaType = MediaType.TEXT_PLAIN))
     public Response updateBommel(@PathParam("id") Long id, @QueryParam("bommelId") Long bommelId) {
-        Optional<TransactionRecord> byIdOptional = repository.findByIdOptional(id);
-
-        if (byIdOptional.isEmpty()) {
-            throw new NotFoundException(Response
-                    .status(Response.Status.NOT_FOUND)
-                    .type(MediaType.TEXT_PLAIN)
-                    .entity("Transaction record not found")
-                    .build());
-        }
+        TransactionRecord transactionRecord = getTransactionRecordAndVerify(id);
+        verifyPermission(bommelId);
 
         try {
             // Just check if bommel is available
@@ -97,13 +129,40 @@ public class TransactionRecordResource {
             throw new WebApplicationException(clientWebApplicationException.getResponse());
         }
 
-        TransactionRecord transactionRecord = byIdOptional.get();
         transactionRecord.setBommelId(bommelId);
         repository.persist(transactionRecord);
 
         return Response
                 .status(Response.Status.CREATED)
                 .build();
+    }
+
+    private TransactionRecord getTransactionRecordAndVerify(Long transactionId) {
+        Optional<TransactionRecord> byIdOptional = repository.findByIdOptional(transactionId);
+        if (byIdOptional.isEmpty()) {
+            throw new NotFoundException(Response
+                    .status(Response.Status.NOT_FOUND)
+                    .type(MediaType.TEXT_PLAIN)
+                    .entity("Transaction record not found")
+                    .build());
+        }
+        TransactionRecord transactionRecord = byIdOptional.get();
+
+        verifyPermission(transactionRecord.getBommelId());
+        return transactionRecord;
+    }
+
+    /**
+     * @throws WebApplicationException
+     *             with {@code jakarta.ws.rs.core.Response.Status.FORBIDDEN} when user is not allowed
+     */
+    private void verifyPermission(Long bommelId) {
+        // Allow seeing if no bommel was set
+        if (bommelId == null) {
+            return;
+        }
+
+        fgaProxy.verifyAccessToBommel(bommelId, securityContext.getUserPrincipal().getName());
     }
 
     public static class AllParameters {
