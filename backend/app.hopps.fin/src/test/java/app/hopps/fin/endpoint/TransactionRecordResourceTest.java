b193cded3692bd66046fd920f2d08d6c9896cf8e
Manuel Klaus <code@codeflowwizardry.de>
2025-01-29 15:41:15 +0100

    ✨ (#269): Introducing OpenFGA to FIN-Service

--- backend/app.hopps.fin/src/test/java/app/hopps/fin/endpoint/TransactionRecordResourceTest.java
+++ backend/app.hopps.fin/src/test/java/app/hopps/fin/endpoint/TransactionRecordResourceTest.java
@@ -1,31 +1,43 @@
 package app.hopps.fin.endpoint;
 
 import app.hopps.commons.DocumentType;
+import app.hopps.commons.fga.FgaRelations;
+import app.hopps.commons.fga.FgaTypes;
 import app.hopps.fin.jpa.TransactionRecordRepository;
 import app.hopps.fin.jpa.entities.TransactionRecord;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.matching.ContainsPattern;
 import com.github.tomakehurst.wiremock.matching.NegativeContainsPattern;
+import io.quarkiverse.openfga.client.AuthorizationModelClient;
+import io.quarkiverse.openfga.client.model.RelObject;
+import io.quarkiverse.openfga.client.model.RelTupleKey;
+import io.quarkiverse.openfga.client.model.RelTupleKeyed;
+import io.quarkiverse.openfga.client.model.RelUser;
 import io.quarkiverse.wiremock.devservice.ConnectWireMock;
 import io.quarkus.panache.common.Page;
+import io.quarkus.test.InjectMock;
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.keycloak.client.KeycloakTestClient;
 import io.quarkus.test.security.TestSecurity;
+import io.smallrye.mutiny.Uni;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import wiremock.com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import wiremock.com.fasterxml.jackson.databind.node.ObjectNode;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlPathMatching;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
 
 @QuarkusTest
 @ConnectWireMock
@@ -34,6 +46,9 @@ import static org.hamcrest.Matchers.is;
 class TransactionRecordResourceTest {
     @Inject
     TransactionRecordRepository repository;
+
+    @InjectMock
+    AuthorizationModelClient authorizationModelClient;
 
     WireMock wireMock;
 
@@ -44,14 +59,43 @@ class TransactionRecordResourceTest {
     void setup() {
         repository.deleteAll();
 
-        TransactionRecord withBommel = new TransactionRecord(BigDecimal.valueOf(50), DocumentType.INVOICE, "alice");
+        TransactionRecord withBommel = new TransactionRecord(BigDecimal.valueOf(50), DocumentType.INVOICE, "test");
         withBommel.setDocumentKey("randomKey");
         withBommel.setBommelId(1L);
         repository.persist(withBommel);
 
-        TransactionRecord noBommel = new TransactionRecord(BigDecimal.valueOf(20), DocumentType.RECEIPT, "alice");
+        TransactionRecord noBommel = new TransactionRecord(BigDecimal.valueOf(20), DocumentType.RECEIPT, "test");
         noBommel.setDocumentKey("randomKey");
         repository.persist(noBommel);
+
+        Mockito.when(authorizationModelClient.check(Mockito.any(RelTupleKeyed.class)))
+                .thenReturn(Uni.createFrom().item(false));
+    }
+
+    @Test
+    void shouldFetchAll() {
+        RelObject relObject = RelObject.of(FgaTypes.BOMMEL.getFgaName(), "1");
+
+        Mockito.when(authorizationModelClient.listObjects(Mockito.any()))
+                .thenReturn(Uni.createFrom().item(List.of(relObject)));
+
+        given()
+                .when()
+                .get("all")
+                .then()
+                .statusCode(200)
+                .body("size()", is(2));
+    }
+
+    @Test
+    void shouldFetchOnlyWithoutBommelId() {
+        given()
+                .when()
+                .get("all")
+                .then()
+                .statusCode(200)
+                .body("size()", is(1))
+                .body("[0].bommelId", nullValue());
     }
 
     @Test
@@ -68,6 +112,15 @@ class TransactionRecordResourceTest {
 
     @Test
     void shouldFetchAllByBommel() {
+        RelTupleKey relTupleKey = RelTupleKey.builder()
+                .user(RelUser.of(FgaTypes.USER.getFgaName(), "test"))
+                .relation(FgaRelations.MEMBER.getFgaName())
+                .object(RelObject.of(FgaTypes.BOMMEL.getFgaName(), "1"))
+                .build();
+
+        Mockito.when(authorizationModelClient.check(relTupleKey))
+                .thenReturn(Uni.createFrom().item(true));
+
         given()
                 .when()
                 .queryParam("bommelId", 1)
@@ -91,22 +144,19 @@ class TransactionRecordResourceTest {
     }
 
     @Test
-    void shouldGetEmptyList() {
+    void shouldGetAForbidden() {
         given()
                 .when()
                 .queryParam("bommelId", 2)
                 .get("all")
                 .then()
-                .statusCode(200)
-                .body("size()", is(0));
+                .statusCode(403);
     }
 
     @Test
     void shouldAddToBommel() {
         // given
         Long id = repository.findWithoutBommel(new Page(0, 10)).getFirst().getId();
-
-        Long bommelId = 1L;
 
         ObjectNode objectNode = JsonNodeFactory.instance.objectNode();
         objectNode
@@ -120,13 +170,22 @@ class TransactionRecordResourceTest {
                                 .withStatus(200)
                                 .withJsonBody(objectNode)));
 
+        RelTupleKey relTupleKey = RelTupleKey.builder()
+                .user(RelUser.of(FgaTypes.USER.getFgaName(), "test"))
+                .relation(FgaRelations.MEMBER.getFgaName())
+                .object(RelObject.of(FgaTypes.BOMMEL.getFgaName(), "1"))
+                .build();
+
+        Mockito.when(authorizationModelClient.check(relTupleKey))
+                .thenReturn(Uni.createFrom().item(true));
+
         // when
         given()
                 .auth()
                 .oauth2(getAccessToken("bob"))
                 .when()
                 .pathParam("id", id)
-                .queryParam("bommelId", bommelId)
+                .queryParam("bommelId", 1)
                 .patch("{id}/bommel")
                 .then()
                 .statusCode(201);
@@ -178,6 +237,15 @@ class TransactionRecordResourceTest {
                         .willReturn(aResponse()
                                 .withStatus(500)));
 
+        RelTupleKey relTupleKey = RelTupleKey.builder()
+                .user(RelUser.of(FgaTypes.USER.getFgaName(), "test"))
+                .relation(FgaRelations.MEMBER.getFgaName())
+                .object(RelObject.of(FgaTypes.BOMMEL.getFgaName(), "1"))
+                .build();
+
+        Mockito.when(authorizationModelClient.check(relTupleKey))
+                .thenReturn(Uni.createFrom().item(true));
+
         // when
         given()
                 .when()
@@ -192,20 +260,13 @@ class TransactionRecordResourceTest {
     void shouldNotAddToBommel() {
         Long id = repository.findWithoutBommel(new Page(0, 10)).getFirst().getId();
 
-        Long bommelId = 99L;
-
-        wireMock.register(
-                get(urlPathMatching("/bommel/99"))
-                        .willReturn(aResponse()
-                                .withStatus(404)));
-
         given()
                 .when()
                 .pathParam("id", id)
-                .queryParam("bommelId", bommelId)
+                .queryParam("bommelId", 99)
                 .patch("{id}/bommel")
                 .then()
-                .statusCode(400);
+                .statusCode(403);
     }
 
     @Test
