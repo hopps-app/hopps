71e0769d4351f735d9ca8f77fb52551cdaeb74ac
Manuel Klaus <code@codeflowwizardry.de>
2025-02-12 23:09:57 +0100

    ✅ (#269): Fix tests

--- backend/app.hopps.org/src/test/java/app/hopps/org/rest/DashboardEndpointTest.java
+++ backend/app.hopps.org/src/test/java/app/hopps/org/rest/DashboardEndpointTest.java
@@ -1,24 +1,37 @@
 package app.hopps.org.rest;
 
+import app.hopps.org.kogito.DataIndexApi;
+import io.quarkus.test.InjectMock;
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.security.TestSecurity;
 import jakarta.ws.rs.core.Response.Status;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
 
 @QuarkusTest
 @TestHTTPEndpoint(DashboardEndpoint.class)
 @TestSecurity(user = "peter")
 class DashboardEndpointTest {
+
+    @InjectMock
+    DataIndexApi dataIndexApi;
+
     @Test
     void shouldGetOpenTasks() {
+        Mockito.when(dataIndexApi.getUserTaskInstances(Mockito.any())).thenReturn(List.of());
+
         given()
                 .when()
                 .get("tasks")
                 .then()
-                .statusCode(Status.OK.getStatusCode());
+                .statusCode(Status.OK.getStatusCode())
+                .body(is("0"));
     }
 
     @Test
