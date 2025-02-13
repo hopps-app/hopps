d09bb27c24bca32659c2d44bbd5a93c2ab3a6f29
Emilia Jaser <vapor.schitcrafter@gmail.com>
2025-02-13 11:48:06 +0100

    ✨ (#154): Implement delegate in fin-service for tagging documents

---
+++ backend/app.hopps.fin/src/test/java/app/hopps/fin/delegates/TagDocumentDelegateTest.java
@@ -0,0 +1,53 @@
+package app.hopps.fin.delegates;
+
+import app.hopps.commons.DocumentType;
+import app.hopps.commons.InvoiceData;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import io.quarkiverse.wiremock.devservice.ConnectWireMock;
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@QuarkusTest
+@ConnectWireMock
+class TagDocumentDelegateTest {
+
+    WireMock wireMock;
+
+    @Inject
+    TagDocumentDelegate delegate;
+
+    @Test
+    void documentTagInterfaceIsCorrect() {
+        // given
+        wireMock.register(
+                post(urlPathMatching("/api/fin-narrator/tag/invoice"))
+                        .withRequestBody(containing("currencyCode"))
+                        .willReturn(aResponse()
+                                .withStatus(200)
+                                .withBody("[\"food\", \"pizza\"]")
+                                .withHeader("Content-Type", "application/json")));
+
+        InvoiceData data = new InvoiceData(
+                -1L,
+                BigDecimal.valueOf(3.0),
+                LocalDate.ofYearDay(2024, 20),
+                "EUR");
+
+        // when
+        var tags = delegate.tagDocument(DocumentType.INVOICE, data);
+
+        // then
+        assertEquals(2, tags.size());
+        assertTrue(tags.contains("food"));
+        assertTrue(tags.contains("pizza"));
+    }
+
+}
