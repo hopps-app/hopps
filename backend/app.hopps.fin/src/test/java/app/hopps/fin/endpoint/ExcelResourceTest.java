e69c8bbe34360e88e79c8b9e5cf99db72750c0e2
Manuel Klaus <code@codeflowwizardry.de>
2025-02-26 00:17:56 +0100

    ✨ (#305): Initial work on excel export

---
+++ backend/app.hopps.fin/src/test/java/app/hopps/fin/endpoint/ExcelResourceTest.java
@@ -0,0 +1,23 @@
+package app.hopps.fin.endpoint;
+
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.security.TestSecurity;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+@QuarkusTest
+@TestSecurity(user = "peter")
+@TestHTTPEndpoint(ExcelResource.class)
+class ExcelResourceTest {
+    @Test
+    void getExcelFile() {
+        given()
+                .when()
+                .get("{bommelId}", 1)
+                .then()
+                .statusCode(200)
+                .contentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+    }
+}
