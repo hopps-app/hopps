0da3fa3b9445d9b8fdc8cb6ddb90f768edb42678
Manuel Klaus <code@codeflowwizardry.de>
2025-03-01 13:43:38 +0100

    ✨ (#305): Modularized columns

--- backend/app.hopps.fin/src/main/resources/db/testdata/V2.0.0__test_data.sql
+++ backend/app.hopps.fin/src/main/resources/db/testdata/V2.0.0__test_data.sql
@@ -3,11 +3,13 @@ VALUES (1, 'Pfaffenhofen an der Ilm', 'Deutschland', 'Joseph-Fraunhofer-Straße 
 ;
 
 INSERT INTO TransactionRecord (total, id, transaction_time, document_key, name, orderNumber, currencyCode, uploader,
-                               privately_paid, document, sender_id)
+                               privately_paid, document, sender_id, bommel_id)
 VALUES (25.0, 1, '2024-12-18T14:00:00.000000Z', 'dummy1', 'hagebaumarkt & Gartencenter', null, 'EUR',
-        'emanuel_urban@domain.none', false, 0, 1),
+        'emanuel_urban@domain.none', false, 0, 1, null),
        (286.58, 2, '2024-12-17T12:24:59.000000Z', 'dummy2', 'hagebaumarkt & Gartencenter', '12345ABC', 'EUR',
-        'emanuel_urban@domain.none', false, 0, 1)
+        'emanuel_urban@domain.none', false, 0, 1, null),
+       (286.58, 3, '2025-02-28T16:59:59.000000Z', 'dummy3', 'hagebaumarkt & Gartencenter', 'ANOTHER', 'USD',
+        'what_zit_tooya@domain.none', false, 0, 1, 2)
 ;
 
-select setval('transaction_sequence', 3, true);
+select setval('transaction_sequence', 4, true);
