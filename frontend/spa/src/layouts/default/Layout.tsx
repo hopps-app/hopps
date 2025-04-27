7bd7e2aeb74dcdd06f914906942d8dcad81e0f41
pweimann <bluubsen@gmail.com>
2025-04-27 11:49:11 +0200

    remove banner

--- frontend/spa/src/layouts/default/Layout.tsx
+++ frontend/spa/src/layouts/default/Layout.tsx
@@ -1,24 +1,18 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Header from './Header.tsx';
 import HomeView from '@/components/views/HomeView.tsx';
 import DemoView from '@/components/views/DemoView.tsx';
-import Banner from './Banner.tsx';
 import NotFoundView from '@/components/views/NotFoundView.tsx';
 import SettingsView from '@/components/views/SettingsView.tsx';
 import AuthGuard from '@/guards/AuthGuard.tsx';
 import { Toaster } from '@/components/ui/shadecn/Toaster.tsx';
 import { RegisterOrganizationView } from '@/components/views/RegisterOrganizationView.tsx';
-import { useStore } from '@/store/store.ts';
 
 function Layout() {
-    const authStore = useStore();
-    const isBannerVisible = !authStore.user;
-
     return (
         <>
             <Router>
-                {isBannerVisible && <Banner />}
                 <div className="flex flex-col mx-auto size-full" style={{ maxWidth: 1360, padding: '0 40px' }}>
                     <Header />
                     <div className="min-h-[calc(100vh-80px)] pb-9">
