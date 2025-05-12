51188cdab969c22e40e0544bdcefb688a688595c
pweimann <bluubsen@gmail.com>
2025-05-12 16:08:09 +0200

    refactor: remove unused code

--- frontend/spa/src/guards/AuthGuard.tsx
+++ frontend/spa/src/guards/AuthGuard.tsx
@@ -1,5 +1,4 @@
-import { ReactNode, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { ReactNode } from 'react';
 
 import { useStore } from '@/store/store.ts';
 import authService from '@/services/auth/auth.service.ts';
@@ -10,27 +9,13 @@ interface AuthGuardProps {
 
 const AuthGuard: React.FC<AuthGuardProps> = ({ children }) => {
     const { isInitialized, isAuthenticated } = useStore();
-    const location = useLocation();
-
-    useEffect(() => {
-        if (isInitialized && !isAuthenticated) {
-            authService.checkLogin();
-        }
-    }, [isInitialized, isAuthenticated]);
 
     if (!isInitialized) {
         return null;
     }
 
     if (!isAuthenticated) {
-        // Store the current path for redirect after login
-        const currentPath = location.pathname + location.search + location.hash;
-        if (currentPath && currentPath !== '/') {
-            window.localStorage.setItem('REDIRECT_PATH', currentPath);
-        }
-
         authService.login();
-
         return null;
     }
 
