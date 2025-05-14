a8efe99c34f4aca73c0f9b71c81424d6e64cf22b
pweimann <bluubsen@gmail.com>
2025-05-14 16:39:24 +0200

    fix: imports after merge conflicts

--- frontend/spa/src/App.tsx
+++ frontend/spa/src/App.tsx
@@ -1,11 +1,11 @@
 import { useEffect } from 'react';
 
 import Layout from '@/layouts/default/Layout.tsx';
-import themeService from '@/services/ThemeService.ts';
 import emojiService from '@/services/EmojiService';
 import languageService from '@/services/LanguageService.ts';
-import authService from '@/services/auth/auth.service.ts';
 import { useStore } from '@/store/store.ts';
+import themeService from '@/services/ThemeService.ts';
+import authService from '@/services/auth/auth.service.ts';
 
 function App() {
     const { isInitialized, setIsInitialized } = useStore();
