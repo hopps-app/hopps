0ed98442597fc391d8dcb0d5d09c5f5245504321
pweimann <bluubsen@gmail.com>
2025-07-12 12:06:18 +0200

    feat: implement new sidebar navigation

---
+++ frontend/spa/src/layouts/default/menu-config.ts
@@ -0,0 +1,27 @@
+import type { RadixIcons } from '@/components/ui/Icon';
+
+export type MenuItem = {
+    id: string;
+    label: string;
+    icon: RadixIcons;
+    path?: string;
+    children?: Omit<MenuItem, 'icon'>[];
+};
+
+export const menuConfig: MenuItem[] = [
+    {
+        id: 'dashboard',
+        label: 'Dashboard',
+        icon: 'Grid',
+        path: '/dashboard',
+        children: [
+            { id: 'receipts', label: 'Receipts', path: '/dashboard/receipts' },
+            { id: 'new-receipt', label: 'New receipt', path: '/dashboard/new-receipt' },
+            { id: 'income', label: 'Income', path: '/dashboard/income' },
+            { id: 'expenses', label: 'Expenses', path: '/dashboard/expenses' },
+        ],
+    },
+    { id: 'receipts', label: 'Receipts', icon: 'Archive', path: '/receipts' },
+    { id: 'structure', label: 'Structure', icon: 'Commit', path: '/structure' },
+    { id: 'admin', label: 'Admin', icon: 'Person', path: '/admin' },
+];
