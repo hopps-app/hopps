38a8745bd65cd5abbf2a86b3c979d26d78eb7099
pweimann <bluubsen@gmail.com>
2025-07-19 10:40:16 +0200

    feat: layouts the user menu, align the usermenu dropdown properly

--- frontend/spa/src/layouts/default/AuthLayout.tsx
+++ frontend/spa/src/layouts/default/AuthLayout.tsx
@@ -1,14 +1,20 @@
 import { Outlet } from 'react-router-dom';
 
 import SidebarNavigation from './sidebar-navigation';
+import UserMenu from '@/layouts/default/UserMenu.tsx';
 
 export default function AuthLayout() {
     return (
         <div className="flex h-screen w-screen bg-violet-50">
             <SidebarNavigation />
-            <main className="flex-1 flex flex-col p-8">
-                <Outlet />
-            </main>
+            <div className="container mx-auto py-8">
+                <header className="flex justify-end">
+                    <UserMenu />
+                </header>
+                <main className="flex-1 flex flex-col">
+                    <Outlet />
+                </main>
+            </div>
         </div>
     );
 }
