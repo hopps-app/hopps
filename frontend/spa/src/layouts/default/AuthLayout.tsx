ee935d31e00977525389dab67a3d0bd0fcc7b908
pweimann <bluubsen@gmail.com>
2025-07-26 11:11:59 +0200

    feat: optimize responsive layout & styling

--- frontend/spa/src/layouts/default/AuthLayout.tsx
+++ frontend/spa/src/layouts/default/AuthLayout.tsx
@@ -7,13 +7,15 @@ export default function AuthLayout() {
     return (
         <div className="flex h-screen bg-background">
             <SidebarNavigation />
-            <div className="container mx-auto py-8">
-                <header className="flex justify-end pb-5">
-                    <UserMenu />
-                </header>
-                <main className="flex-1 flex flex-col">
-                    <Outlet />
-                </main>
+            <div className="flex-1 flex justify-center ml-0 sm:ml-28 bg-red-900">
+                <div className="w-full max-w-[1360px] px-4 py-8 bg-white">
+                    <header className="flex justify-end pb-5">
+                        <UserMenu />
+                    </header>
+                    <main className="flex-1 flex flex-col">
+                        <Outlet />
+                    </main>
+                </div>
             </div>
         </div>
     );
