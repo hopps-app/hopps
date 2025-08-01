93bf963d95229669c9a141a1cd0ed291649a85e6
Emilia Jaser <vapor.schitcrafter@gmail.com>
2025-08-01 17:58:37 +0200

    ✅ (#217): Pass tests

--- backend/app.hopps.org/src/test/java/app/hopps/fin/endpoint/DocumentResourceTest.java
+++ backend/app.hopps.org/src/test/java/app/hopps/fin/endpoint/DocumentResourceTest.java
@@ -1,6 +1,9 @@
 package app.hopps.fin.endpoint;
 
+import app.hopps.fin.bpmn.SubmitService;
+import app.hopps.fin.jpa.entities.TransactionRecord;
 import app.hopps.fin.model.DocumentType;
+import io.quarkus.test.InjectMock;
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.security.TestSecurity;
@@ -12,6 +15,7 @@ import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
@@ -26,6 +30,9 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 @TestHTTPEndpoint(DocumentResource.class)
 class DocumentResourceTest {
     private static final String IMAGE_KEY = "imageKey";
+
+    @InjectMock
+    SubmitService submitServiceMock;
 
     @Inject
     Flyway flyway;
@@ -74,17 +81,23 @@ class DocumentResourceTest {
 
     @Test
     void shouldUploadFile() {
-
         InputStream zugferdInputStream = getClass().getClassLoader().getResourceAsStream("ZUGFeRD.pdf");
         assertNotNull(zugferdInputStream);
 
+        var transaction = new TransactionRecord();
+
+        Mockito.when(submitServiceMock.submitDocument(Mockito.any(SubmitService.DocumentSubmissionRequest.class)))
+                .thenReturn(transaction);
+
         given()
                 .contentType(MediaType.MULTIPART_FORM_DATA)
-                .multiPart("file", "ZUGFeRD.pdf", zugferdInputStream)
-                .multiPart("type", DocumentType.INVOICE.toString())
+                .multiPart("file", "ZUGFeRD.pdf", zugferdInputStream, "application/pdf")
+                .multiPart("type", DocumentType.INVOICE.toString(), "text/plain")
+                .multiPart("privatelyPaid", true, "text/plain")
+                .multiPart("bommelId", 1, "text/plain")
                 .when()
                 .post()
                 .then()
-                .statusCode(Response.Status.ACCEPTED.getStatusCode());
+                .statusCode(Response.Status.OK.getStatusCode());
     }
 }
