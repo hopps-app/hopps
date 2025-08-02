a1532f7bcb410f5616e2dcb5e2d7d9d74988bd1c
Emilia Jaser <vapor.schitcrafter@gmail.com>
2025-08-02 11:44:30 +0200

    ✨ (#217): Uploading a document without a bommel now defaults to the root bommel

--- backend/app.hopps.org/src/test/java/app/hopps/fin/endpoint/DocumentResourceTest.java
+++ backend/app.hopps.org/src/test/java/app/hopps/fin/endpoint/DocumentResourceTest.java
@@ -15,21 +15,30 @@ import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatcher;
 import org.mockito.Mockito;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.io.InputStream;
+import java.util.Objects;
 
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
 
 @QuarkusTest
-@TestSecurity(user = "peter")
+@TestSecurity(user = "alice@example.test")
 @TestHTTPEndpoint(DocumentResource.class)
 class DocumentResourceTest {
     private static final String IMAGE_KEY = "imageKey";
+    /// ID of the root bommel from the test user's organisation
+    /// User: alice@example.test.
+    /// Org: slug=buehnefrei-ev, id=3.
+    /// see V2.0.0__test_data.sql
+    private static final int ROOT_BOMMEL_ID = 23;
 
     @InjectMock
     SubmitService submitServiceMock;
@@ -86,7 +95,7 @@ class DocumentResourceTest {
 
         var transaction = new TransactionRecord();
 
-        Mockito.when(submitServiceMock.submitDocument(Mockito.any(SubmitService.DocumentSubmissionRequest.class)))
+        Mockito.when(submitServiceMock.submitDocument(any(SubmitService.DocumentSubmissionRequest.class)))
                 .thenReturn(transaction);
 
         given()
@@ -94,10 +103,82 @@ class DocumentResourceTest {
                 .multiPart("file", "ZUGFeRD.pdf", zugferdInputStream, "application/pdf")
                 .multiPart("type", DocumentType.INVOICE.toString(), "text/plain")
                 .multiPart("privatelyPaid", true, "text/plain")
-                .multiPart("bommelId", 1, "text/plain")
+                .multiPart("bommelId", 30, "text/plain")
                 .when()
                 .post()
                 .then()
                 .statusCode(Response.Status.OK.getStatusCode());
+    }
+
+    @Test
+    void shouldUploadToRootBommel() {
+        InputStream zugferdInputStream = getClass().getClassLoader().getResourceAsStream("ZUGFeRD.pdf");
+        assertNotNull(zugferdInputStream);
+
+        var transaction = new TransactionRecord();
+
+        ArgumentMatcher<SubmitService.DocumentSubmissionRequest> matchesRequest = (
+                SubmitService.DocumentSubmissionRequest req) -> req.bommelId() == ROOT_BOMMEL_ID
+                && req.type() == DocumentType.INVOICE
+                && req.privatelyPaid()
+                && Objects.equals(req.submitterUserName(), "alice@example.test")
+                && Objects.equals(req.contentType(), "application/pdf");
+
+        Mockito.when(submitServiceMock.submitDocument(argThat(matchesRequest)))
+                .thenReturn(transaction);
+
+        given()
+                .contentType(MediaType.MULTIPART_FORM_DATA)
+                .multiPart("file", "ZUGFeRD.pdf", zugferdInputStream, "application/pdf")
+                .multiPart("type", DocumentType.INVOICE.toString(), "text/plain")
+                .multiPart("privatelyPaid", true, "text/plain")
+                .when()
+                .post()
+                .then()
+                .statusCode(Response.Status.OK.getStatusCode());
+    }
+
+    @Test
+    void shouldFailOnInvalidBommel() {
+        InputStream zugferdInputStream = getClass().getClassLoader().getResourceAsStream("ZUGFeRD.pdf");
+        assertNotNull(zugferdInputStream);
+
+        var transaction = new TransactionRecord();
+
+        Mockito.when(submitServiceMock.submitDocument(any(SubmitService.DocumentSubmissionRequest.class)))
+                .thenReturn(transaction);
+
+        given()
+                .contentType(MediaType.MULTIPART_FORM_DATA)
+                .multiPart("file", "ZUGFeRD.pdf", zugferdInputStream, "application/pdf")
+                .multiPart("type", DocumentType.INVOICE.toString(), "text/plain")
+                .multiPart("privatelyPaid", true, "text/plain")
+                .multiPart("bommelId", 999999999, "text/plain")
+                .when()
+                .post()
+                .then()
+                .statusCode(Response.Status.BAD_REQUEST.getStatusCode());
+    }
+
+    @Test
+    void shouldFailOnBommelFromAnotherOrganization() {
+        InputStream zugferdInputStream = getClass().getClassLoader().getResourceAsStream("ZUGFeRD.pdf");
+        assertNotNull(zugferdInputStream);
+
+        var transaction = new TransactionRecord();
+
+        Mockito.when(submitServiceMock.submitDocument(any(SubmitService.DocumentSubmissionRequest.class)))
+                .thenReturn(transaction);
+
+        given()
+                .contentType(MediaType.MULTIPART_FORM_DATA)
+                .multiPart("file", "ZUGFeRD.pdf", zugferdInputStream, "application/pdf")
+                .multiPart("type", DocumentType.INVOICE.toString(), "text/plain")
+                .multiPart("privatelyPaid", true, "text/plain")
+                .multiPart("bommelId", 2, "text/plain")
+                .when()
+                .post()
+                .then()
+                .statusCode(Response.Status.BAD_REQUEST.getStatusCode());
     }
 }
