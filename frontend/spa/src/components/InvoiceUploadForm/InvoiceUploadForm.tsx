88e73b511a7c57414a60f95f59e3d8bd34ee56a8
s0582346 <s0582346@htw-berlin.de>
2025-08-24 22:39:42 +0200

    styled upload invoice

--- frontend/spa/src/components/InvoiceUploadForm/InvoiceUploadForm.tsx
+++ frontend/spa/src/components/InvoiceUploadForm/InvoiceUploadForm.tsx
@@ -44,45 +44,48 @@ const InvoiceUploadForm: FC<InvoiceUploadType> = ({ onUploadInvoiceChange }) => 
     ];
 
     return (
-        <form onSubmit={handleSubmit} className="invoice-upload-form flex flex-col gap-4">
+        <form onSubmit={handleSubmit} className="invoice-upload-form">
             {isUploading && <LoadingOverlay />}
-            <span className="font-normal text-md text-start">{t('invoiceUpload.title')}</span>
-            <div className="md:flex flex-row gap-5 flex">
-                <InvoiceUploadFormDropzone onFilesChanged={onFilesChanged} />
 
-                <div className="md:flex flex-col gap-5 flex flex-1">
-                    <div className="overflow-y-auto max-h-[200px] ">
+            <div className="grid grid-cols-[1fr_2fr] gap-6 h-full">
+                <div className="min-w-0 min-h-0 min-w-[400px]">
+                    <InvoiceUploadFormDropzone onFilesChanged={onFilesChanged} />
+                </div>
+
+                <div className="min-w-0 min-h-0 min-w-[300px]">
+                    <div className="min-h-0 flex-1 overflow-y-auto">
                         <List
-                            items={selectedFiles.map((file, index) => ({
+                            items={selectedFiles.map((file, i) => ({
                                 title: file.name,
-                                id: file.name + index,
+                                id: file.name + i,
                                 progress: fileProgress[file.name] || 0,
                                 icon: 'File',
                                 iconSize: 'md',
                             }))}
-                            isRemovableListItem={true}
-                            className="gap-2 "
+                            isRemovableListItem
+                            className="gap-2"
                             onClickRemove={onClickRemoveSelected}
                         />
                     </div>
-                    {isInvoiceSizeLimit && <p className="text-red-500  font-medium text-xs">{t('invoiceUpload.totalTooLarge')}</p>}
-                    {isInvoicesQuantityLimit && <p className="text-red-500  font-medium text-xs">{t('invoiceUpload.maxFilesExceeded')}</p>}
-                    <div className="flex gap-2 flex-col">
+
+                    {isInvoiceSizeLimit && <p className="text-red-500 text-xs font-medium">{t('invoiceUpload.totalTooLarge')}</p>}
+                    {isInvoicesQuantityLimit && <p className="text-red-500 text-xs font-medium">{t('invoiceUpload.maxFilesExceeded')}</p>}
+
+                    <div className="flex flex-col gap-2">
                         <div>{t('invoiceUpload.assign')}</div>
                         <div className="flex flex-col gap-8">
                             <InvoiceUploadFormBommelSelector onChange={(a) => a && onBommelSelected(a)} />
-                            {errors.bommelId && <p className="text-red-500 font-medium text-xs">{errors.bommelId.message}</p>}
+                            {errors.bommelId && <p className="text-red-500 text-xs font-medium">{errors.bommelId.message}</p>}
+
                             <div className="flex flex-col gap-2">
                                 <h6 className="text-sm font-medium">{t('invoiceUpload.docType')}</h6>
                                 <Select className="h-16 rounded-2xl" value={documentType} onValueChanged={onDocumentTypeChange} items={transactionTypes} />
-                                {errors.documentType && <p className="text-red-500 font-medium text-xs">{errors.documentType.message}</p>}
+                                {errors.documentType && <p className="text-red-500 text-xs font-medium">{errors.documentType.message}</p>}
                             </div>
+
                             <div className="flex items-center space-x-2">
                                 <Checkbox checked={isPrivatelyPaid} onCheckedChange={handleCheckboxChange} id="privatelyPaid" />
-                                <label
-                                    htmlFor="privatelyPaid"
-                                    className="text-xs font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                                >
+                                <label htmlFor="privatelyPaid" className="text-xs font-medium leading-none">
                                     {t('invoiceUpload.privately')}
                                 </label>
                             </div>
