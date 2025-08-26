7dd87e6be12d6248f7c52a9c6476e92bc5c80aa8
pweimann <bluubsen@gmail.com>
2025-08-26 11:07:08 +0200

    refactor: removed the pin/lock behavior from the desktop sidebar

--- frontend/spa/src/components/sidebar-navigation/index.tsx
+++ frontend/spa/src/components/sidebar-navigation/index.tsx
@@ -4,21 +4,10 @@ import DesktopSidebar from './desktop-sidebar';
 import MobileSidebar from './mobile-sidebar';
 
 const SidebarNavigation: React.FC = () => {
-    const [isWideScreen, setIsWideScreen] = React.useState(false);
-
-    React.useEffect(() => {
-        const checkScreenWidth = () => {
-            setIsWideScreen(window.innerWidth > 1400);
-        };
-
-        checkScreenWidth();
-        window.addEventListener('resize', checkScreenWidth);
-    }, []);
-
     return (
         <>
             <div className="hidden sm:block">
-                <DesktopSidebar isWideScreen={isWideScreen} />
+                <DesktopSidebar />
             </div>
             <div className="sm:hidden">
                 <MobileSidebar />
