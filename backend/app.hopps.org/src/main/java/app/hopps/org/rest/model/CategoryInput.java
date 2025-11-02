0c46d25132cbbecf6f5ef98584759bf9a1841176
Manuel Hummler <manuel@hummler.com>
2025-11-02 19:45:22 +0000

    🚨 fix linter issue

--- backend/app.hopps.org/src/main/java/app/hopps/org/rest/model/CategoryInput.java
+++ backend/app.hopps.org/src/main/java/app/hopps/org/rest/model/CategoryInput.java
@@ -6,7 +6,7 @@ import org.hibernate.validator.constraints.Length;
 
 @Schema(name = "CategoryInput", description = "Input for creating a new category")
 public record CategoryInput(
-        @NotBlank @Length(min = 1, max =  127) @Schema(description = "Name of the category", example = "Technology") String name,
+        @NotBlank @Length(min = 1, max = 127) @Schema(description = "Name of the category", example = "Technology") String name,
 
         @Length(max = 255) @Schema(description = "Description of the category", example = "Category for technology-related items") String description) {
 }
