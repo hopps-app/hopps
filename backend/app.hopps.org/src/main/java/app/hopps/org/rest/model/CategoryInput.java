ba7319b73c202a5177c6f0142bcdedd74a3f605b
Manuel Hummler <manuel@hummler.com>
2025-11-02 19:16:09 +0000

    🧑‍💻 improvements of Emilias Feedback

--- backend/app.hopps.org/src/main/java/app/hopps/org/rest/model/CategoryInput.java
+++ backend/app.hopps.org/src/main/java/app/hopps/org/rest/model/CategoryInput.java
@@ -2,10 +2,11 @@ package app.hopps.org.rest.model;
 
 import jakarta.validation.constraints.NotBlank;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.hibernate.validator.constraints.Length;
 
 @Schema(name = "CategoryInput", description = "Input for creating a new category")
 public record CategoryInput(
-        @NotBlank @Schema(description = "Name of the category", example = "Technology") String name,
+        @NotBlank @Length(min = 1, max =  127) @Schema(description = "Name of the category", example = "Technology") String name,
 
-        @Schema(description = "Description of the category", example = "Category for technology-related items") String description) {
+        @Length(max = 255) @Schema(description = "Description of the category", example = "Category for technology-related items") String description) {
 }
--- backend/app.hopps.org/src/main/resources/db/migration/V1.0.2__category_table.sql
+++ backend/app.hopps.org/src/main/resources/db/migration/V1.0.2__category_table.sql
@@ -1,9 +1,9 @@
-create sequence Category_SEQ start with 1 increment by 50;
+create sequence Category_SEQ start with 1 increment by 1;
 
 create table Category
 (
     id              bigint not null primary key,
     organization_id bigint not null references Organization,
-    name            varchar(255) not null,
+    name            varchar(127) not null,
     description     varchar(255)
 );
