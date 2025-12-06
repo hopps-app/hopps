4049b31cc01e76acad24ec34f74f0311f7f21bb3
Manuel Hummler <manuel@hummler.com>
2025-12-06 13:12:01 +0100

    ♻️ remove unused IT test files and refactor TransactionRecordResourceTest for Flyway-based data setup

--- backend/app.hopps.org/src/test/java/app/hopps/member/api/MemberResourceTestsIT.java
+++
@@ -1,8 +0,0 @@
-package app.hopps.member.api;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class MemberResourceTestsIT extends MemberResourceTests {
-    // marker subclass only
-}
--- backend/app.hopps.org/src/test/java/app/hopps/organization/api/OrganizationResourceTestsIT.java
+++
@@ -1,8 +0,0 @@
-package app.hopps.organization.api;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class OrganizationResourceTestsIT extends OrganizationResourceTests {
-    // marker subclass only
-}
