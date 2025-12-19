7447acf5f500cf2cc8376ea2a29f7fc558874af9
Markus Herhoffer <markus@herhoffer.net>
2025-12-19 18:50:41 +0100

    Use formatter

--- fuggs-app/src/main/java/rest/Todos.java
+++ fuggs-app/src/main/java/rest/Todos.java
@@ -14,50 +14,64 @@ import io.quarkiverse.renarde.Controller;
 import model.Todo;
 
 /**
- * This defines a REST controller, each method will be available under the "Classname/method" URI by convention
+ * This defines a REST controller, each method will be available under the
+ * "Classname/method" URI by convention
  */
-public class Todos extends Controller {
-    
-    /**
-     * This defines templates available in src/main/resources/templates/Classname/method.html by convention
-     */
-    @CheckedTemplate
-    public static class Templates {
-        /**
-         * This specifies that the Todos/index.html template does not take any parameter
-         */
-        public static native TemplateInstance index();
-        /**
-         * This specifies that the Todos/todos.html template takes a todos parameter of type List&lt;Todo&gt;
-         */
-        public static native TemplateInstance todos(List<Todo> todos);
-    }
+public class Todos extends Controller
+{
 
-    // This overrides the convention and makes this method available at "/renarde"
-    @Path("/renarde")
-    public TemplateInstance index() {
-        // renders the Todos/index.html template
-        return Templates.index();
-    }
+	/**
+	 * This defines templates available in
+	 * src/main/resources/templates/Classname/method.html by convention
+	 */
+	@CheckedTemplate
+	public static class Templates
+	{
+		/**
+		 * This specifies that the Todos/index.html template does not take any
+		 * parameter
+		 */
+		public static native TemplateInstance index();
 
-    public TemplateInstance todos() {
-        // renders the Todos/todos.html template with our list of Todo entities
-        return Templates.todos(Todo.listAll());
-    }
-    
-    // Creates a POST action at Todos/add taking a form element named task
-    @POST
-    public void add(@RestForm @NotBlank String task) {
-        // If validation fails, redirect to the todos page (with errors propagated)
-        if(validationFailed()) {
-            // redirect to the todos page by just calling the method: it does not return!
-            todos();
-        }
-        // save the new Todo
-        Todo todo = new Todo();
-        todo.task = task;
-        todo.persist();
-        // redirect to the todos page
-        todos();
-    }
+		/**
+		 * This specifies that the Todos/todos.html template takes a todos
+		 * parameter of type List&lt;Todo&gt;
+		 */
+		public static native TemplateInstance todos(List<Todo> todos);
+	}
+
+	// This overrides the convention and makes this method available at
+	// "/renarde"
+	@Path("/renarde")
+	public TemplateInstance index()
+	{
+		// renders the Todos/index.html template
+		return Templates.index();
+	}
+
+	public TemplateInstance todos()
+	{
+		// renders the Todos/todos.html template with our list of Todo entities
+		return Templates.todos(Todo.listAll());
+	}
+
+	// Creates a POST action at Todos/add taking a form element named task
+	@POST
+	public void add(@RestForm @NotBlank String task)
+	{
+		// If validation fails, redirect to the todos page (with errors
+		// propagated)
+		if (validationFailed())
+		{
+			// redirect to the todos page by just calling the method: it does
+			// not return!
+			todos();
+		}
+		// save the new Todo
+		Todo todo = new Todo();
+		todo.task = task;
+		todo.persist();
+		// redirect to the todos page
+		todos();
+	}
 }
--- fuggs-app/src/main/java/util/Startup.java
+++ fuggs-app/src/main/java/util/Startup.java
@@ -10,21 +10,24 @@ import io.quarkus.runtime.StartupEvent;
 import model.Todo;
 
 @ApplicationScoped
-public class Startup {
-    /**
-     * This method is executed at the start of your application
-     */
-    public void start(@Observes StartupEvent evt) {
-        // in DEV mode we seed some data
-        if(LaunchMode.current() == LaunchMode.DEVELOPMENT) {
-            Todo a = new Todo();
-            a.task = "First item";
-            a.persist();
+public class Startup
+{
+	/**
+	 * This method is executed at the start of your application
+	 */
+	public void start(@Observes StartupEvent evt)
+	{
+		// in DEV mode we seed some data
+		if (LaunchMode.current() == LaunchMode.DEVELOPMENT)
+		{
+			Todo a = new Todo();
+			a.task = "First item";
+			a.persist();
 
-            Todo b = new Todo();
-            b.task = "Second item";
-            b.completed = new Date();
-            b.persist();
-        }
-    }
+			Todo b = new Todo();
+			b.task = "Second item";
+			b.completed = new Date();
+			b.persist();
+		}
+	}
 }
