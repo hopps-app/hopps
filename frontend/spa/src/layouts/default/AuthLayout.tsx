f69760780b762ab88dbd292d6ce88dffeeb8a641
s0582346 <paoloobregon11@outlook.com>
2025-12-26 19:23:58 +0100

    readjust page content

--- frontend/spa/src/layouts/default/AuthLayout.tsx
+++ frontend/spa/src/layouts/default/AuthLayout.tsx
@@ -1,28 +1,25 @@
 import { Outlet } from 'react-router-dom';
 
-import UserMenu from '@/layouts/default/UserMenu.tsx';
+import UserMenu from '@/layouts/default/UserMenu';
 import SidebarNavigation from '@/components/sidebar-navigation';
 
 export default function AuthLayout() {
     return (
         <div className="flex h-screen bg-background">
             <SidebarNavigation />
-            <div className="flex-1 ml-0 sm:ml-28 flex">
-                <div className="m-5 sm:m-6 flex-1">
-                    <div className="flex justify-end px-6 py-4">
-                        <UserMenu />
-                    </div>
 
-                    <main className="p-4 sm:p-6">
-                        <div
-                            className="
-                	            min-h-[80vh]
-                	            w-full"
-                        >
-                            <Outlet />
-                        </div>
-                    </main>
+            <div className="flex-1 sm:ml-28 flex flex-col px-7">
+                {/* Top bar */}
+                <div className="flex justify-end py-3">
+                    <UserMenu />
                 </div>
+
+                {/* Main content */}
+                <main className="flex-1 p-4 sm:p-6">
+                    <div className="h-[80vh] w-full">
+                        <Outlet />
+                    </div>
+                </main>
             </div>
         </div>
     );
