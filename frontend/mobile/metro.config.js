9d1046f936a2adcab3a3031e579d5c3e5234014a
Manuel Hummler <manuel@hummler.com>
2026-01-09 21:33:24 +0100

    ✨ (mobile): make hopss mobile app work on android

--- frontend/mobile/metro.config.js
+++ frontend/mobile/metro.config.js
@@ -1,9 +1,6 @@
-const { getDefaultConfig } = require("expo/metro-config");
+const { getDefaultConfig } = require("@expo/metro-config");
 const { withNativeWind } = require('nativewind/metro');
 
-// Get the default Metro configuration
-let config = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
 
-
-// Export the combined configuration
 module.exports = withNativeWind(config, { input: './global.css' });
