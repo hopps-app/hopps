8527d8be73d046aa9e94322d6c943637321228cc
Manuel Hummler <manuel@hummler.com>
2026-01-09 16:07:19 +0100

    ✨(mobile): Add mobile client again

---
+++ frontend/mobile/constants/Colors.ts
@@ -0,0 +1,26 @@
+/**
+ * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
+ * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
+ */
+
+const tintColorLight = '#B583DA';
+const tintColorDark = '#8D41C6';
+
+export const Colors = {
+  light: {
+    text: '#0A0A0A',
+    background: '#F5EEFA',
+    tint: tintColorLight,
+    icon: '#687076',
+    tabIconDefault: '#687076',
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: '#ECEDEE',
+    background: '#151718',
+    tint: tintColorDark,
+    icon: '#9BA1A6',
+    tabIconDefault: '#9BA1A6',
+    tabIconSelected: tintColorDark,
+  },
+};
