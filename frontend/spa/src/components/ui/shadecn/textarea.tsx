c97345e3b32ba1b1d460cbcc84866c3f48d12482
s0582346 <paoloobregon11@outlook.com>
2026-02-24 22:23:32 +0300

    refactor: standardize border and border-radius styles in BaseInput and Textarea components

--- frontend/spa/src/components/ui/shadecn/textarea.tsx
+++ frontend/spa/src/components/ui/shadecn/textarea.tsx
@@ -6,8 +6,8 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, React.ComponentProps<'tex
     return (
         <textarea
             className={cn(
-                'flex min-h-[80px] w-full bg-primary-foreground rounded-[10px] border border-[#A7A7A7] px-3 py-2 text-base shadow-sm placeholder:text-muted-foreground',
-                'focus:border-primary focus:outline-none focus:border-[2px] focus-visible:ring-1 focus-visible:ring-primary',
+                'flex min-h-[80px] w-full bg-primary-foreground rounded-xl border border-[#d1d5db] px-3 py-2 text-base shadow-sm placeholder:text-muted-foreground',
+                'focus:border-primary focus:outline-none focus:ring-1 focus:ring-primary',
                 'disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
                 className
             )}
