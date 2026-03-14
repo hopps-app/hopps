5d7e5d5c2861eef8f2fe599dc99eb1315083acf7
Manuel Hummler <manuel@hummler.com>
2026-03-14 20:51:16 +0100

    💄 Fix prettier issues

--- frontend/spa/src/components/ OrganizationRegistrationSuccess/OrganizationRegistrationSuccess.tsx
+++ frontend/spa/src/components/ OrganizationRegistrationSuccess/OrganizationRegistrationSuccess.tsx
@@ -14,9 +14,7 @@ export function OrganizationRegistrationSuccess() {
     return (
         <div className="flex flex-col items-center justify-center py-12 px-4">
             <CheckmarkIcon className="w-16 h-16 mb-6" />
-            <h2 className="text-lg font-semibold text-black mb-6 text-center">
-                {t('organization.registration.successHeading')}
-            </h2>
+            <h2 className="text-lg font-semibold text-black mb-6 text-center">{t('organization.registration.successHeading')}</h2>
             <Button onClick={onClickLogin}>{t('header.login')}</Button>
         </div>
     );
