5912980cf8cfabf78e06c755985792eac9851668
Manuel Hummler <manuel@hummler.com>
2026-03-18 20:59:29 +0100

    🐛 fix typescript issues

--- frontend/spa/src/components/InvoiceUploadForm/hooks/useUploadForm.ts
+++ frontend/spa/src/components/InvoiceUploadForm/hooks/useUploadForm.ts
@@ -221,7 +221,7 @@ export function useUploadForm({ onUploadInvoiceChange }: InvoiceUploadType) {
                 // Upload all files
                 const uploadResults = await Promise.all(
                     selectedFiles.map((file) =>
-                        apiService.orgService.documentsPOST({
+                        apiService.orgService.documentsPOST(undefined, {
                             data: file,
                             fileName: file.name,
                         })
