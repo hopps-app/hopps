077ea533ba1030b74ea5a7a7772d1feec70d4a88
s0582346 <paoloobregon11@outlook.com>
2026-03-19 16:14:38 +0300

    update hover effect in UserMenu

--- frontend/spa/src/layouts/default/UserMenu.tsx
+++ frontend/spa/src/layouts/default/UserMenu.tsx
@@ -25,7 +25,7 @@ function UserMenu() {
         isAuthenticated && (
             <div>
                 <DropdownMenu items={menuItems} className="w-56">
-                    <div className="flex flex-row items-center gap-2 px-3 py-2 rounded-lg cursor-pointer transition-colors hover:bg-muted dark:hover:bg-primary">
+                    <div className="flex flex-row items-center gap-2 px-3 py-2 rounded-lg cursor-pointer transition-colors hover:bg-hover-effect dark:hover:bg-purple-200">
                         <div className="flex-shrink-0">
                             <PersonIcon className="w-5 h-5" />
                         </div>
